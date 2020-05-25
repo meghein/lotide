20d834d5bdc99dd80753adcf38dc4541cb8f5b00
Meghan Hein <meghan.hein@gmail.com>
2020-05-25 22:56:02 +0000

    proper syntax

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,12 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   } else {
-    return `❌❌❌ Assertion Failed: ${actual} !== ${expected}`
+    return `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
   }
 };
 
 // TEST CODE
 console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual(1, "1"));
+console.log(assertEqual(1, 1));
