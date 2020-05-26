a5d54ea047c1b6c67d06d41048b2cef4238cd801
Meghan Hein <meghan.hein@gmail.com>
2020-05-26 23:37:04 +0000

    created a function to remove array elements based on secondary array elements

---
+++ without.js
@@ -0,0 +1,41 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `❌❌❌ Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const without = function(source, itemsToRemove) {
+  let result = [];
+  for (let i = 0; i < source.length; i++) {
+    for (let j = 0; j < itemsToRemove.length; j++) {
+      if (source[i] !== itemsToRemove[j]) {
+        result.push(source[i]);
+      }
+    }
+  }
+  return result;
+};
+
+// TEST CODE
+without([1, 2, 3], [1]); // => [2, 3]
+without(["1", "2", "3"], [1, 2, "3"]); // => ["1", "2"]
+
+console.log(without([1, 2, 3], [1]));
+console.log(assertArraysEqual(without([1, 2, 3], [1]), ([2, 3])));
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
