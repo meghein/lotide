2aa6328bfdabf8dd6e6bb27a4d1cf025c693ab3b
Meghan Hein <meghan.hein@gmail.com>
2020-05-26 22:45:55 +0000

    updated code

--- eqArrays.js
+++ eqArrays.js
@@ -7,7 +7,10 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length; ++i) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) return false;
   }
   return true;
@@ -17,12 +20,7 @@ const eqArrays = function(arr1, arr2) {
 
 //TEST CODE
 
-eqArrays([1, 2, 3], [1, 2, 3]); // => true
-eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
-
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false));
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true));
 console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false));
 console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true));
