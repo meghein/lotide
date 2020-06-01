2ba742b2f559e1b4b73c88ae9c7efb66608839c3
Meghan Hein <meghan.hein@gmail.com>
2020-06-01 18:39:47 +0000

    added index file with utility function library with implemented object shorthand

---
+++ index.js
@@ -0,0 +1,28 @@
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const head   = require('./head');
+const letterPosition = require('./letterPosition');
+const map = require('./map');
+const middle = require('./middle');
+const takeUntil = require('./takeUntil');
+const tail   = require('./tail');
+const without = require('./without') 
+
+
+module.exports = {
+  countLetters,
+  countOnly,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPosition,
+  map,
+  middle,
+  takeUntil,
+  tail,
+  without
+};
--- test/tailTest.js
+++ test/tailTest.js
@@ -5,11 +5,11 @@ const tail = require('../tail')
 describe("#tail", () => {
 
   it("returns 3 for ['Hello', 'Yo', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(tail(["Hello", "Yo", "Lighthouse", "Labs"]).length, 3)
+    assert.deepEqual(tail(["Hello", "Yo", "Lighthouse", "Labs"]), ["Yo", "Lighthouse", "Labs"])
   })
 
   it("returns 2 for [' Yo Yo', 'Lighthouse', 'Labs']", () => {
-    assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2)
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"])
   })
 
 });
