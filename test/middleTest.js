e9df6ceca53abedbb8a873bfe1003e4113f8694d
Meghan Hein <meghan.hein@gmail.com>
2020-06-01 18:00:10 +0000

    refactoring midde and tail test with mocha and chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,25 @@
-const assertArraysEqual = require('../assertArraysEqual');
+//const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert
 const middle = require('../middle')
 
+describe("#middle", () => {
+
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [5, 6] for [4, 5, 6, 7]", () => {
+    assert.deepEqual(middle([4,5,6,7]), [5,6]);
+  });
+
+  it("returns [30] for [10, 20, 30, 40, 50]", () => {
+    assert.deepEqual(middle([10, 20, 30, 40, 50]), [30]);
+  });
+
+});
+
 // TEST CODE
-console.log(assertArraysEqual(middle([5,6,7]), [6]));
-console.log(assertArraysEqual(middle([5,6,7,8]), [6,7]));
-console.log(assertArraysEqual(middle([5,6,7,8,9]), [7]));
+// console.log(assertArraysEqual(middle([5,6,7]), [6]));
+// console.log(assertArraysEqual(middle([5,6,7,8]), [6,7]));
+// console.log(assertArraysEqual(middle([5,6,7,8,9]), [7]));
 
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,20 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+const assert = require('chai').assert
 const tail = require('../tail')
 
+describe("#tail", () => {
+
+  it("returns 3 for ['Hello', 'Yo', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(["Hello", "Yo", "Lighthouse", "Labs"]).length, 3)
+  })
+
+  it("returns 2 for [' Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2)
+  })
+
+});
+
+
 // TEST CODE
-console.log(assertEqual(tail(["Hello", "Yo", "Lighthouse", "Labs"]).length, 3));
-console.log(assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2));
+// console.log(assertEqual(tail(["Hello", "Yo", "Lighthouse", "Labs"]).length, 3));
+// console.log(assertEqual(tail(["Yo Yo", "Lighthouse", "Labs"]).length, 2));
